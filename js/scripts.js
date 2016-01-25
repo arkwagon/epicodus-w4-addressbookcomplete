9f164a78054e71dfc24d370e91257b8ec9ebd4a7
Joshua Gustafson and Will Murphy <wilmurph@gmail.com>
2016-01-25 09:30:35 -0800

    final commit for part 1

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,25 @@ Contact.prototype.fullName = function() {
 }
 
 
-//
-// $(document).ready(function() {
-//
-//   $("form").submit(function(event){
-//
-//     event.preventDefault();
-//   });
-//
-// });
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
