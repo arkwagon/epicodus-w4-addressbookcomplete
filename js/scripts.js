5fb358ae149602290703da7121da058c03b78a10
Schuyler Klaassen and Will Murphy <schuyler.m.k@gmail.com>
2016-01-26 10:50:19 -0800

    Add html for address input fields

--- js/scripts.js
+++ js/scripts.js
@@ -26,18 +26,38 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedKind = $("input#new-address-kind").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newAddress = new Address(inputtedKind, inputtedStreet, inputtedCity, inputtedState);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    if (inputtedFirstName === "" || inputtedLastName === "" || inputtedKind === "" || inputtedStreet === "" || inputtedCity === "" || inputtedState=== "") {
+      alert("Please complete all fields!");
+    } else {
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-    });
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+      $(".contact").last().click(function() {
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.fullName());
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+        $("#show-contact h3").text(newAddress.kind);
+        $(".street").text(newAddress.street);
+        $(".city").text(newAddress.city);
+        $(".state").text(newAddress.state);
+
+
+      });
+  //Reset Fields to blank
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#new-address-kind").val("");
+      $("input#new-street").val("");
+      $("input#new-city").val("");
+      $("input#new-state").val("");
+    }
   });
 });
